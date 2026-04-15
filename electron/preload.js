be97c58032b68698442ca9dcddaf71337f62789e
Geordan Gesink <georgegeordan@gmail.com>
2026-04-15 18:21:03 +0200

    on windows after udate applied, shut app down

--- electron/preload.js
+++ electron/preload.js
@@ -4,7 +4,8 @@ contextBridge.exposeInMainWorld('bridge', {
     return ipcRenderer.sendSync('pkg')
   },
   applyUpdate: () => ipcRenderer.invoke('pear:applyUpdate'),
-  appRestart: () => ipcRenderer.invoke('app:restart'),
+  appAfterUpdate: () => ipcRenderer.invoke('app:afterUpdate'),
+  appShutdown: () => ipcRenderer.invoke('app:shutdown'),
   onPearEvent: (name, listener) => {
     const wrap = (evt, eventName) => listener(eventName)
     ipcRenderer.on('pear:event:' + name, wrap)
